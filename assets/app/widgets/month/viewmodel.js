eb0a420f1b97b35ec7e51c9f1c43bc5188a07c0d
DaveGee <david.geretti@gmail.com>
2015-01-23 15:02:40 +0100

    month widget initialisation

--- assets/app/widgets/month/viewmodel.js
+++ assets/app/widgets/month/viewmodel.js
@@ -1,15 +1,33 @@
-define(['moment'], function(moment) {
-  var monthWidget = function() {
+define(['moment'], function (moment) {
+    var monthWidget = function () {
+        this.month = moment();
+        this.days = [];
 
-    this.month = 0;
-    this.year = moment().year();
+        this.monthNb = this.month.month();
+        this.year = this.month.year();
 
-  };
+    };
 
-  monthWidget.prototype.active = function (settings) {
-    this.month = settings.month;
-    this.year = settings.year || moment().year;
-  };
+    monthWidget.prototype.activate = function (settings) {
+        this.monthNb = Math.min(settings.month, 11);
+        this.year = settings.year || moment().year();
 
-  return monthWidget;
+        this.month = moment({
+                year: this.year,
+                month: this.monthNb
+            });
+
+        var day = this.month.startOf('month').clone(),
+            dayEnd = this.month.endOf('month').clone();
+
+        while(!day.isAfter(dayEnd, 'day')) {
+            this.days.push({
+                dayInMonth: day.date(),
+                weekDay: day.day()
+            });
+            day.add(1, 'd');
+        }
+    };
+
+    return monthWidget;
 });
